479123f438e7ee77c759411a9ee8057a7b6fb93d
Igor Santos <igorsantoscosta@gmail.com>
2022-10-18 12:57:28 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/global.css";
+import {ExamplePDF} from './pages/pdf'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
     <App />
-    {/* <ExamplePDF /> */}
+    <ExamplePDF />
   </>
 );
