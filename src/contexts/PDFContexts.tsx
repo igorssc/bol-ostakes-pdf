9440fbccbb7681c31e13e07627c8a9439507dc80
Igor Santos <igorsantoscosta@gmail.com>
2022-10-19 13:41:40 -0300

    Update participants.json

--- src/contexts/PDFContexts.tsx
+++ src/contexts/PDFContexts.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode } from "react";
 import data from "../../data.json";
-import participants from "../../participants1.json";
+import participants from "../../participants-copy.json";
 
 interface PDFProviderProps {
   children: ReactNode;
